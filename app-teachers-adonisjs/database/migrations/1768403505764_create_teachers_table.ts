f74bb47265ef368018d9acc18959df414c406e6e
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-01-14 16:31:59 +0100

    feat(app-teachers): étape 3 finit
    [35] [DONE]

---
+++ app-teachers-adonisjs/database/migrations/1768403505764_create_teachers_table.ts
@@ -0,0 +1,34 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'teachers'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.enum('gender', ['woman', 'man', 'other']).notNullable().defaultTo('woman')
+      // Champ prénom
+      table.string('firstname').notNullable()
+      // Champ nom
+      table.string('lastname').notNullable()
+      // Champ surnom
+      table.string('nickname').notNullable()
+      // Champ origine du prénom
+      table.string('origine').notNullable()
+      // Clé étrangère de la section
+      table
+      .integer('section_id') // Clé étrangère
+      .unsigned() // La clé ne doit pas être négative
+      .references('id') // Référence la colonne `id` de la table `section`
+      .inTable('sections') // Nom de la table de référence
+      .onDelete('CASCADE') // Supprime les teachers si la section est supprimée
+      .onUpdate('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
