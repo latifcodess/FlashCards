fcb9249c86e11ae36f651b3945440ae016b37249
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-01-21 13:32:31 +0100

    feat(app-teachers): étape 5 finit
    [15] [DONE]

---
+++ app-teachers-adonisjs/database/seeders/teacher_seeder.ts
@@ -0,0 +1,36 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Teacher from '#models/teacher'
+import { TeacherFactory } from '#database/factories/teacher_factory'
+
+export default class extends BaseSeeder {
+  async run() {
+    await Teacher.createMany([
+      {
+        gender: 'man',
+        firstname: 'Grégory',
+        lastname: 'Charmier',
+        nickname: 'GregLeBarbar',
+        origine: "Plateforme de jeux d'échecs",
+        sectionId: 1,
+      },
+      {
+        gender: 'man',
+        firstname: 'Xavier',
+        lastname: 'Carrel',
+        nickname: 'XCL',
+        origine: 'Sigle ETML',
+        sectionId: 1,
+      },
+      {
+        gender: 'woman',
+        firstname: 'Aurélie',
+        lastname: 'Curchod',
+        nickname: 'ACD',
+        origine: 'Sigle ETML',
+        sectionId: 1,
+      },
+    ])
+
+    await TeacherFactory.createMany(10)
+  }
+}
