a14c9c5a23882694e035fb76d366c6251f0b040d
LatifKrasniqi24 <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-02-05 18:26:49 +0100

    feat(src): modification du style
    [20] [DONE]

--- src/database/seeders/deck_seeder.ts
+++ src/database/seeders/deck_seeder.ts
@@ -5,7 +5,15 @@ export default class extends BaseSeeder {
   async run() {
     await Deck.createMany([
       {id: 1, title: 'Vue.js', description: 'Framework Vue.js avec mr Charmier'},
-      {id: 2, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'}
+      {id: 2, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 3, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 4, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 5, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 6, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 7, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 8, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 9, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
+      {id: 10, title: 'MongoDB', description: 'Base de données NoSQL avec mr Meylan'},
     ])
   }
 }
