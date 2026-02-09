4fce505e50137d7857f9dc2be2621770448389b6
LatifKrasniqi24 <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-02-09 18:19:41 +0100

    feat(src): ajout d'un bouton d'ajout de deck sans logique
    [10] [Done]

--- src/start/routes.ts
+++ src/start/routes.ts
@@ -9,6 +9,9 @@
 
 import DecksController from '#controllers/decks_controller'
 import deck_seeder from '#database/seeders/deck_seeder'
+import Deck from '#models/deck'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', [DecksController, 'index']).as('home')
+router.get('/deck/add', [DecksController, 'create']).as('deck.create')
+router.post('/deck/add', [DecksController, 'store']).as('deck.store')
